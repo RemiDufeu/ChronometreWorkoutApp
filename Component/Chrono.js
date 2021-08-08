086e8e7e5fff4605c8649c1a5b2a60a27621cb20
RemiDufeu <dufeu.remi@gmail.com>
2021-08-08 15:27:13 +0200

    test apk

--- Component/Chrono.js
+++ Component/Chrono.js
@@ -1,9 +1,10 @@
 import React, { Component, useState } from 'react';
 import { useEffect } from 'react';
-import { View, Text, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Button, TouchableOpacity, StyleSheet, Modal } from 'react-native';
 import AppContext from '../Context/AppContext'
 import timeToString from '../utils/timeToString';
 import { useFonts } from 'expo-font';
+import Sound from 'react-native-sound';
 
 function Chrono({ navigation }) {
 
@@ -19,9 +20,15 @@ function Chrono({ navigation }) {
 
 function ChronoJs ({minutes, secondes, navigation}) {
     
+    const bip = new Sound('../assets/bip.wav', null, (error) => {
+        if (error) {
+          console.error(error)
+        }
+    })
 
     let [fontsLoaded] = useFonts({
-        'Sarpanch': require('../assets/fonts/Sarpanch-ExtraBold.ttf'),
+        'ChakraPetch-Bold': require('../assets/fonts/ChakraPetch-Bold.ttf'),
+        
     });
 
     console.log(fontsLoaded)
@@ -53,6 +60,7 @@ function ChronoJs ({minutes, secondes, navigation}) {
                 if (secondesTimer == 0 && status) {
                     if (minutesTimer == 0) {
                         clearInterval(timerInterval)
+                        bip.play()
                         setPause()
                         resetTimer()
                     } else {
@@ -66,28 +74,47 @@ function ChronoJs ({minutes, secondes, navigation}) {
               }
         },
     );
-    const pauseView = <>
-        <TouchableOpacity  onPress={()=> { setResume()}}>
-            <Text>Resume</Text>
+    const pauseView = <Modal
+        animationType="slide"
+        transparent={true}
+        visible={!status}
+        style={{ height : '100%', width : '100%', position :'absolute', elevation : 10}}>
+            <View style={{ height : '100%', width : '100%', backgroundColor : 'rgba(0, 0, 0, 0.8)', position : 'absolute',}}>
+        <TouchableOpacity  onPress={()=> { setResume()}} style={{ height : '85%', width : '100%', textAlign : 'center', alignItems : 'center', justifyContent : 'center'}}>
+            <View stryl = {{flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center', 
+		alignItems: 'center'}}>
+            <Text style={{width: '100%', textAlign : 'center', color : 'white', fontWeight: "800", fontSize : 160, paddingRight : 30}}> GO</Text>
+            </View>
+            
         </TouchableOpacity>
-        { (minutesTimer != minutes || secondesTimer != secondes) && <TouchableOpacity activeOpacity={0.6} onLongPress={() => { resetTimer()}}>
-            <Text >
-              Restart
+        { (minutesTimer != minutes || secondesTimer != secondes) && <TouchableOpacity activeOpacity={0.6} style={{position : 'absolute', bottom : 0, width : '100%'}} onLongPress={() => { resetTimer()}}>
+            <Text style={{width: '100%', backgroundColor : 'rgb(33, 150, 243)', textAlign : 'center', color : 'white', padding : 16,fontSize : 40, fontWeight: "500"}} >
+                RESTART
             </Text>
         </TouchableOpacity> }
-        </>
+            </View>
+        </Modal>
         
     
 
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            {fontsLoaded ?<TouchableOpacity onPress={() => setPause()} >
-                <Text style={{fontFamily : 'Sarpanch', fontSize : 100}}> {timeToString(minutesTimer)}</Text>
-                <Text style={{fontFamily : 'Sarpanch' , fontSize : 100}}> {timeToString(secondesTimer)}</Text>
+        <View style={{
+            justifyContent: "center",
+            alignItems: "center",
+            height : '100%'
+         }}>
+        {!status &&  pauseView }
+        <View>
+            {fontsLoaded ?<TouchableOpacity onPress={() => setPause()} style={{height : '100%', justifyContent : 'center'}}>
+                <Text style={{fontFamily : 'ChakraPetch-Bold', lineHeight : 200, fontSize : 200, textAlign :'center', width :"100%", paddingRight : 50, position: 'relative'}}> {timeToString(minutesTimer)}</Text>
+                <Text style={{fontFamily : 'ChakraPetch-Bold' , lineHeight : 200, fontSize : 200, textAlign :'center', width :"100%", paddingRight : 50, position: 'relative'}}> {timeToString(secondesTimer)}</Text>
             </TouchableOpacity> : <Text>Chargement...</Text>}
             
-            {!status &&  pauseView }
+            
+        </View>
         </View>
     )
 }
